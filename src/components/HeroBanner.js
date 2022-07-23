f8e53d9428c5d3b5fdeec5e5a5ec6f524ac6f038
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-07-24 01:15:16 +0300

    Home

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+const HeroBanner = () => {
+  return (
+    <div>
+      <Box>
+        
+      </Box>
+    </div>
+  )
+}
+
+export default HeroBanner
