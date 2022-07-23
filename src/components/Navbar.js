819d51eece9e8d5c30ac5169bdbfd325860b5a26
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-07-24 00:57:12 +0300

    add mui/material

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react' 
+import { Link } from 'react-router-dom'
+import { Stack } from 'mui/material'
+import Logo from '../assets/images/Logo.png'
 
 const Navbar = () => {
   return (
     <div>
-      
+      Navbar
     </div>
   )
 }
