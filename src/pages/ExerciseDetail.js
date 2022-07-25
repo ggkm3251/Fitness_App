68267831cb3f96d4f271b876801172c47e60a6e1
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-07-25 15:21:00 +0300

    api item details

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -4,7 +4,7 @@ import { Box } from '@mui/material';
 
 import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
-//import ExerciseVideos from '../components/ExerciseVideos';
+import ExerciseVideos from '../components/ExerciseVideos';
 //import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
@@ -42,7 +42,7 @@ const ExerciseDetail = () => {
   return (
     <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
       <Detail exerciseDetail={exerciseDetail} />
-      {/*<ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />*/}
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
       {/*<SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />*/}
     </Box>
   );
