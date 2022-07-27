9fd1c34ae1e59aed006ad32812bdae3ad473fe4d
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-07-27 22:37:15 +0300

    Add mui material and jsx

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -38,7 +38,9 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-      
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
+        Awesome Exercises You <br /> Should Know
+      </Typography>
       <Box position="relative" mb="72px" mt='10px'>
         <TextField
           height="76px"
